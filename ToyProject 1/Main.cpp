00a3f80dc0e8b7463bb95a8d875e63fecc9284a9
ParkSeyang <ghlwkd14@naver.com>
2025-04-14 17:52:28 +0900

    (2025.04.14) 참조자를 사용한 함수

--- ToyProject 1/Main.cpp
+++ ToyProject 1/Main.cpp
@@ -14,7 +14,10 @@
 */
 #include <iostream>
 #include <Windows.h> // 커서의 위치를 옮기는 함수 SetCursorPosition
-#include <conio.h> // _getch 입력 대기함수
+#include <conio.h>   // _getch 입력 대기함수
+#include <string>    // 문자열의 기능을 포함하고 있는 라이브러리
+#include <iomanip>   // 약자 (Input Output Manipulate 라이브러리)
+
 namespace ConsoleUtils
 {
 	void GoToXY(int x, int y)
@@ -64,8 +67,113 @@ namespace ConsoleUtils
 
 }
 
+struct Item
+{
+	// char name[20]; std::string 은 이름의 크기를 자동으로 할당해준다.
+	std::string name;
+	int price;
+};
+
+// Item 구조체의 참조자를 받아와서 이름을 원하는 이름으로 변경하고 가격을 원하는 가격으로 설정하는 함수를 만든다.
+
+// const 사용, 참조자 사용, Item 구조체 사용
+
+
+void MySetItem(Item& itemname,const std::string name,int Price)
+{
+	std::cout << "해당 " << name << " 의 가격 : " << Price << std::endl;
+
+}
+
+
+void SetItem(Item& item, const std::string Nname, int price)
+{
+	item.name = Nname; // 외부에서 가져온 item을 Nname으로 설정해준다.
+	item.price = price;
+}
+
+void SetItem(Item& Setitem, const Item& copyItem) // ShopItem 항목,
+{
+	Setitem.name = copyItem.name;
+	Setitem.price = copyItem.price;
+}
+// default 매개 인자: 함수 호출 시에 매개 변수를 부족하게 입력하면 자동으로 변수에 대입된다.
+// ShowShop(Shop, 3,10,10);
+// Shop, length << default가 아니기 때문에 앞부분에 작성 해줘야 된다
+void ShowShop(Item shop[], int length, int x = 10, int y = 10)
+{
+	ConsoleUtils::GoToXY(x, y);
+	// #include <iomanip> std::left, right 왼쪽, 오른쪽 정렬
+	// std:: setw(20); 문자열의 크기가 20공간을 만든다. 문자열의 빈공간은 공백 문자로 출력한다.
+	std::cout <<std::left <<std::setw(20) << "아이템 이름" << "|" 
+		<< std::setw(10) << "아이템 가격" << std::endl;
+
+	for (int i = 0; i < length; i++)
+	{
+		// std::cout << "이름 : " << ItemA.name << " ,  가격 : " << ItemA.price << std::endl;
+		// std::cout << "이름 : " << shop[i].name << " ,  가격 : " << shop[i].price << std::endl;
+		ConsoleUtils::GoToXY(x, y + 1 + i); // 표의 틀보다 1칸아래에 있어야 하기때문에 1을 더해줌 i만큼더해준다.
+		std::cout << std::left << std::setw(20) << shop[i].name << "|" 
+			<< std::setw(10) << shop[i].price << "|" << std::endl;
+	}
+}
+
+//shop배열, 선택한 인덱스, item 배열 인벤토리.
+// copyItem 
+// 상점(Item 배열)에 있는 데이터를 복사해서, 유저의 아이템 배열에 가져온다. 배열의 데이터를 변경한다.
+
+Item& GetItem(Item shop[], int length, int index)
+{
+	if (index >= length)
+	{
+		std::cout << " 등록되지 않은 아이템을 선택하여 0번째 아이템이 반환됩니다." << std::endl;
+		return shop[0];
+	}
+	return shop[index];
+}
+
+void AddInventory(Item inventory[], int length, int index, const Item& buyItem)
+{
+	inventory[index].name = buyItem.name;
+	inventory[index].price = buyItem.price;
+}
 int main()
 {
+	std::cout << " 아이템 예제 " << std::endl;
+	
+	Item ItemA;
+	Item ItemB;
+	Item ItemC;
+
+	SetItem(ItemA, "Apple", 100);
+	SetItem(ItemB, "Banana", 500);
+	SetItem(ItemC, "Candy", 50);
+
+	Item Shop[3]{ ItemA,ItemB,ItemC };
+
+	Item Inventory[3];
+
+	ShowShop(Shop,3,0,0);
+	std::cout << "구매하고 싶은 아이템 번호를 입력해주세요 " << std::endl;
+	int input;
+	std::cin >> input;
+
+	AddInventory(Inventory, 3, 0, GetItem(Shop, 3, input - 1));
+	// 사용 예시
+	// 상점으로 부터 인벤토리에 아이템을 추가하는 것.
+	// 몬스터 데이터가 여러개가 있는데 현재 배틀 중에 등장할 몬스터를 선택한다.
+	// 몬스터 데이터를 불러오는거
+	// Stage정보가 있는데 특정 스테이지를 호출하고 싶다.
+
+	std::cout << "인벤토리의 아이템 확인 " << std::endl;
+	std::cout << Inventory[0].name << "," << Inventory[0].price << std::endl;
+
+	// SetItem(ItemA, "Apple", 100);
+	// SetItem(ItemB, ItemA);
+	// std::cout << "이름 : " << ItemA.name << " ,  가격 : " << ItemA.price << std::endl;
+	// std::cout << "이름 : " << ItemB.name << " ,  가격 : " << ItemB.price << std::endl;
+	_getch();
+
 	// 예제 1. 10,10 좌표에 플레이어의 정보 텍스트를 출력해보세요.
 	ConsoleUtils::GoToXY(10, 10);
 	std::cout << "플레이어 의 정보";
